48e0b5906d227070d93a9acaa3e4c43bae53f11c
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-04-11 12:40:19 +0000

    added

--- contact-form/contact-form-poo.php
+++ contact-form/contact-form-poo.php
@@ -48,9 +48,14 @@ class Contact_form
 {
     public function __construct()
     {
+        // add menu icon
         add_action('admin_menu', array($this, 'contact_form_add_menu'));
+        // load css, js ets
         add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+        // add shortcode
         add_shortcode('contact-form', array($this, 'load_shortcode'));
+        // add jquery
+        add_action('wp_footer', array($this, 'load_script'));
     }
 
     public function contact_form_add_menu()
@@ -86,9 +91,30 @@ class Contact_form
     }
 
     public function load_shortcode()
-    {
-        return "hello, ilias is the best";
-    }
+    { ?>
+        <div class="contact-form">
+            <h3 class="mb-5">Send us a message</h3>
+            <p class="danger mb-5">please fill the below form</p>
+            <form id="contact-form__form">
+                <input name="First_name" class="mb-5 w-100" type="text" placeholder="First name" required="">
+                <input name="Last" class="mb-5 w-100" type="text" placeholder="Last name" required="">
+                <input name="Subject" class="mb-5 w-100" type="text" placeholder="Subject" required="">
+                <input name="email" class="mb-5 w-100" type="email" placeholder="E-mail" required="">
+                <textarea class="mb-5 w-100" name="message" id="message" placeholder="Type your message" required=""></textarea>
+                <button type="submit" class="btn btn-send w-100">Send message</button>
+            </form>
+        </div>
+    <?php }
+    public function load_script()
+    { ?>
+        <script>
+            (function ($) {
+                $('#contact-form__form').submit(function (event) {
+                    alert('ilias did a good work')
+                })
+            })(jQuery)
+        </script>
+    <?php }
 }
 
 new Contact_form;
