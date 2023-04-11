5b96e1ce9667b81f157ca5b4f3441a85af815d08
ilias anouar <ilas.anouar.solicode@gmail.com>
2023-04-11 09:59:50 +0000

    added

--- contact-form/contact-form-poo.php
+++ contact-form/contact-form-poo.php
@@ -10,29 +10,29 @@ if (!defined('ABSPATH')) {
     exit;
 }
 // Create the table in the database when the plugin is activated
-// register_activation_hook(__FILE__, 'contact_form_create_table');
+register_activation_hook(__FILE__, 'contact_form_create_table');
 
-// function contact_form_create_table()
-// {
-//     global $wpdb;
+function contact_form_create_table()
+{
+    global $wpdb;
 
-//     $table_name = $wpdb->prefix . 'contact_form';
-//     $charset_collate = $wpdb->get_charset_collate();
+    $table_name = $wpdb->prefix . 'contact_form';
+    $charset_collate = $wpdb->get_charset_collate();
 
-//     $sql = "CREATE TABLE $table_name (
-//       id mediumint(9) NOT NULL AUTO_INCREMENT,
-//       sujet varchar(255) NOT NULL,
-//       nom varchar(255) NOT NULL,
-//       prenom varchar(255) NOT NULL,
-//       email varchar(255) NOT NULL,
-//       message text NOT NULL,
-//       date_envoi datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-//       UNIQUE KEY id (id)
-//   ) $charset_collate;";
+    $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      sujet varchar(255) NOT NULL,
+      nom varchar(255) NOT NULL,
+      prenom varchar(255) NOT NULL,
+      email varchar(255) NOT NULL,
+      message text NOT NULL,
+      date_envoi datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+      UNIQUE KEY id (id)
+  ) $charset_collate;";
 
-//     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-//     dbDelta($sql);
-// }
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
 // Delete the table from the database when the plugin is deactivated
 register_deactivation_hook(__FILE__, 'contact_form_delete_table');
 
@@ -44,36 +44,14 @@ function contact_form_delete_table()
     $wpdb->query("DROP TABLE IF EXISTS $table_name");
 }
 
-class contact_form
+class Contact_form
 {
     public function __construct()
     {
-        register_activation_hook(__FILE__, 'contact_form_create_table');
         add_action('admin_menu', array($this, 'contact_form_add_menu'));
-
-
+        add_action('wp_enqueue_scripts', array($this, 'load_assets'));
     }
-    public function contact_form_create_table()
-    {
-        global $wpdb;
 
-        $table_name = $wpdb->prefix . 'contact_form';
-        $charset_collate = $wpdb->get_charset_collate();
-
-        $sql = "CREATE TABLE $table_name (
-      id mediumint(9) NOT NULL AUTO_INCREMENT,
-      sujet varchar(255) NOT NULL,
-      nom varchar(255) NOT NULL,
-      prenom varchar(255) NOT NULL,
-      email varchar(255) NOT NULL,
-      message text NOT NULL,
-      date_envoi datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-      UNIQUE KEY id (id)
-  ) $charset_collate;";
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
-    }
     public function contact_form_add_menu()
     {
         add_menu_page(
@@ -82,13 +60,23 @@ class contact_form
             'manage_options',
             plugin_dir_path(__FILE__) . 'admin/view.php',
             null,
-            'dashicons-admin-tools',
+            'dashicons-welcome-widgets-menus',
             20
         );
     }
+
+    public function load_assets()
+    {
+        wp_enqueue_style(
+            'contact-form',
+            plugin_dir_url(__FILE__).'css/contact-form.css',
+            array(),
+            'all'
+    );
+    }
 }
 
-new contact_form;
+new Contact_form;
 
 
 ?>
